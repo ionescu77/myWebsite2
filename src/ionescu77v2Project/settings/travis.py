1f131f4191f77d54bc52cf1891e0be6e0d1cba16
razgitbot <ionescu77@gmail.com>
2020-05-31 14:48:43 +0300

    Fix Application labels aren't unique, duplicates: landing

--- src/ionescu77v2Project/settings/travis.py
+++ src/ionescu77v2Project/settings/travis.py
@@ -9,34 +9,10 @@ DISQUS = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
-INSTALLED_APPS += (
-    'landing',
-    'blogengine',
-    'accounts',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.syndication',
-    'django.contrib.sitemaps',
-    'crispy_forms',
-    'axes',
-)
-
 SITE_ID = 1
 
 #TEST_DATABASE_CHARSET=UTF8
 #CHARSET=UTF8 # supported for PG and MySQL only
-
-# ///////
-# ------- django-axes: CACHES enabled, 20181119
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    },
-    'axes_cache': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    }
-}
-# ------- #
 
 INSTALLED_APPS += ('django_jenkins',)
 JENKINS_TASKS = ()
@@ -47,12 +23,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # ///////
 # ------- django-axes: Settings for this:
-AXES_CACHE = 'axes_cache'
-AXES_VERBOSE = True
-AXES_LOCKOUT_TEMPLATE = 'lockout.html'
-AUTHENTICATION_BACKENDS = [
-    'axes.backends.AxesModelBackend',
-    'django.contrib.auth.backends.ModelBackend',
-]
-
+AXES_VERBOSE = False
 # ------- #
